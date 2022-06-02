75e59507e16f65081c17abfce69ab03879937307
2ok-k <52870159+2ok-k@users.noreply.github.com>
2022-06-02 10:32:08 +0000

    maj

--- src/main/java/com/miage/gestion_etudiants/controllers/EtudiantController.java
+++ src/main/java/com/miage/gestion_etudiants/controllers/EtudiantController.java
@@ -1,17 +1,26 @@
 package com.miage.gestion_etudiants.controllers;
 
+import com.miage.gestion_etudiants.entities.Etudiant;
 import com.miage.gestion_etudiants.repositories.EtudiantRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/Etudiant")
 public class EtudiantController {
     private EtudiantRepository etudiantRepository;
 
-    @GetMapping("/index")
-    public String index(){
+    @RequestMapping("/index")
+    public String index(Model model){
+        List<Etudiant> etudiants = etudiantRepository.findAll();
+        model.addAttribute("etudiants",etudiants);
         return "etudiants";
     }
 }
